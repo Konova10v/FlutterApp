e72afe24ec47c917f01e22124ec96ac4f87bbb80
konovalov_k <konova10v@yandex.ru>
2022-07-01 13:56:43 +0500

    project

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kBackground = Color(0xFFf4eedd);
+const kHeaderColor = Color(0xFFeca965);
+const kButtonColor = Color(0xFF267e7f);
---
+++ lib/profile_view.dart
@@ -0,0 +1,292 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class ProfileView extends StatefulWidget {
+  const ProfileView({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: kBackground,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          child: SizedBox(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                      color: kHeaderColor,
+                      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+                    ),
+                    padding: EdgeInsets.fromLTRB(10, 40, 10, 20),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(onPressed: (){
+
+                            }, icon: Icon(Icons.menu, color: Colors.black, size: 30,)),
+
+                            IconButton(onPressed: (){
+
+                            }, icon: Icon(Icons.search, color: Colors.black, size: 30,)),
+                          ],
+                        ),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.supervised_user_circle, color: Colors.white, size: 100,),
+
+                            Column(
+                              children: [
+                                Text('Kirill Konovalov', style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25,
+                                  color: Colors.black,
+                                ),),
+
+                                Text('Mobile Developer', style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.brown,
+                                ),),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                ),
+
+                Container(
+                  padding: EdgeInsets.fromLTRB(25, 30, 10, 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('My Tasks', style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),),
+
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: kButtonColor,
+                          shape: CircleBorder(),
+                          padding: EdgeInsets.all(10),
+                        ),
+                        onPressed: (){
+                          setState(() {
+
+                          });
+                        },
+                        child: Icon(Icons.calendar_today, color: Colors.white,),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TaskCell(
+                          name: 'To Do',
+                          progress: '5 task now. 1 started.',
+                          color: Colors.redAccent,
+                          icon: Icon(Icons.access_time_rounded, color: Colors.white),),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TaskCell(
+                          name: 'In Progress',
+                          progress: '1 task now. 1 started.',
+                          color: kHeaderColor,
+                          icon: Icon(Icons.circle_outlined, color: Colors.white),),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TaskCell(
+                          name: 'Done',
+                          progress: '10 task now. 18 started.',
+                          color: Colors.deepPurple,
+                          icon: Icon(Icons.done, color: Colors.white),),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  padding: EdgeInsets.fromLTRB(25, 30, 10, 10),
+                  child: Row(
+                    children: [
+                      Text('Active Projects', style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    ],
+                  ),
+                ),
+
+                GridView.count(
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  padding: EdgeInsets.all(20),
+                  childAspectRatio: 1/1.3 ,
+                  crossAxisSpacing: 30,
+                  mainAxisSpacing: 30,
+                  physics: NeverScrollableScrollPhysics(), children: [
+                  ProjectCell(
+                    name: 'Medical App',
+                    progress: 0.2,
+                    color: kButtonColor,
+                    time: '9 hours progress',),
+                  ProjectCell(
+                    name: 'Fitness App',
+                    progress: 0.8,
+                    color: Colors.redAccent,
+                    time: '5 hours progress',),
+                  ProjectCell(
+                    name: 'Fuck team',
+                    progress: 1.0,
+                    color: kHeaderColor,
+                    time: '99 hours progress',),
+                  ProjectCell(
+                    name: 'Another support App',
+                    progress: 0.5,
+                    color: Colors.deepPurple,
+                    time: '1 hours progress',),
+                ],),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TaskCell extends StatelessWidget {
+  final String name;
+  final String progress;
+  final Color color;
+  final Icon icon;
+
+  const TaskCell({
+    required this.name,
+    required this.progress,
+    required this.color,
+    required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: color,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(10),
+            ),
+            onPressed: (){
+
+            },
+            child: icon),
+
+        SizedBox(width: 10,),
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(name, style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),),
+
+            Text(progress),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ProjectCell extends StatelessWidget {
+  final String name;
+  final String time;
+  final Color color;
+  final double progress;
+
+  const ProjectCell({
+    required this.name,
+    required this.time,
+    required this.color,
+    required this.progress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(height: 0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(width: 20,),
+              CircularPercentIndicator(
+                radius: 40.0,
+                lineWidth: 8.0,
+                percent: progress,
+                center: Text(printText(progress), style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),),
+                progressColor: Colors.white,
+              ),
+            ],
+          ),
+
+          Wrap(
+            direction: Axis.horizontal, //Vertical || Horizontal
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(name, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(time, style: TextStyle(color: Colors.white,),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+        ],
+      ),
+    );
+  }
+
+  String printText (double progress) {
+    String test = "";
+    double result = progress * 100;
+    int procent = result.round();
+    return '$procent%';
+  }
+}
